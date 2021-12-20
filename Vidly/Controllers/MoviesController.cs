098d99fd67a165c66891d59d1e6af010b29483e6
Md Jahirul Islam <jahirbd350@gmail.com>
2021-12-20 14:28:08 +0600

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movies = new Movies() { Name = "Sharks!" };
+            return View(movies);
+        }
+    }
+}
